0a0508e6a0e52606072a2b227a84c8532340cc21
james-acsc <165851955+james-acsc@users.noreply.github.com>
2026-02-04 10:34:07 +1100

    feat: load new Azul mascot (#38)
    This PR introduces the new Azul mascot. It bounces rather than spins.
    
    It is slightly taller than the old loader but my testing indicates this
    isn't an issue.

--- src/app/common/loading-indicator-failed/loading-indicator-failed.component.ts
+++ src/app/common/loading-indicator-failed/loading-indicator-failed.component.ts
@@ -7,5 +7,5 @@ import { Component, Input } from "@angular/core";
 })
 export class LoadingIndicatorFailedComponent {
   @Input()
-  height: string = "100px";
+  height: string = "125px";
 }
--- src/app/common/loading-indicator/loading-indicator.component.ts
+++ src/app/common/loading-indicator/loading-indicator.component.ts
@@ -3,12 +3,11 @@ import { Component, Input } from "@angular/core";
 @Component({
   selector: "az-loading-indicator",
   templateUrl: "./loading-indicator.component.html",
-  styleUrls: ["./loading-indicator.component.css"],
   standalone: false,
 })
 export class LoadingIndicatorComponent {
   @Input()
   animated = true;
   @Input()
-  height: string = "100px";
+  height: string = "125px";
 }
